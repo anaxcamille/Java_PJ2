84dffd558a9f848107c34675bde1edd24283d0f7
Ana Camille Toledo <74693547+anaxcamille@users.noreply.github.com>
2021-01-22 19:47:38 +0800

    Update ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class ArraySum {
+public class Reverse {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
