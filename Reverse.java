6ed2abbf44e3109bf74c4944b7856d8cc3c5c162
Ana Camille Toledo <74693547+anaxcamille@users.noreply.github.com>
2021-01-22 19:48:32 +0800

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Reverse {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+	     System.out.print("\nInput a word: ");
+		 String word = in.nextLine();
+		 word = word.trim();
+		 String result = ""; 
+	     char[] ch=word.toCharArray();  
+			 for (int i = ch.length - 1; i >= 0; i--) {
+				 result += ch[i];
+			 }
+			 String reverse = result.trim();
+			if(reverse.equals(word)){
+				System.out.print("yes");
+			}else{
+				System.out.print("No");
+			} 
+		 }			 
+        
+ }
